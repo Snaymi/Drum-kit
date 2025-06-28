199f57c3a06175eec3de5f05abd091e79f309d80
Snaymi <sneymeb@hotmail.com>
2025-06-28 14:14:54 -0300

    updadete

--- drum.js
+++ drum.js
@@ -15,7 +15,6 @@ function playSound(e) {
   audio.play();
   console.log(key);
   key.classList.add('playing');
-  setTimeout(() => key.classList.remove('playing'), 10000); // Remove the class after 100ms
 }
 function removeTransition(e) {
     console.log(e);
